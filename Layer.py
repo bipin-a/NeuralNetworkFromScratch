0b20a8c0cd17208a59b06e7463dbc9ae416e516c
aasibipin <aasibipin@gmail.com>
2020-08-02 18:32:15 -0400

    updates

--- Layer.py
+++ Layer.py
@@ -1,4 +1,3 @@
-# Three neurons with 4 inputs.
 import numpy as np
 
 # Layer 1:
@@ -11,25 +10,10 @@ biases = [2, 3, 0.5]
 
 # Layer 2: 
 # Input: 3x3, Neurons: 3
-weights2 = [ [0.1, -0.14, 0.5], [-0.5, 0.12, -0.33], [-0.44, 0.73, -0.13]]
-# Removed one neruon per weight, because the input is now 3x3.
+weights2 = [ [0.1, -0.14, 0.5], [-0.5, 0.12, -0.33], [-0.44, 0.73, -0.13]]      # Removed one neruon per weight, because the input is now 3x3.
 biases2 = [-1, 2, -0.5]
         
 layer1_outputs = np.dot(inputs,np.array(weights).T) + biases    #Outputs 3x3
 layer2_outputs = np.dot(layer1_outputs,np.array(weights2).T) + biases2
 
 print (layer2_outputs)
-
-
-
-# Old Fashion Method
-'''
-output_layer = []
-for n_weights, n_bias in zip(weights, biases):
-    n_output = 0
-    for input, weight in zip(inputs, n_weights):      
-        n_output += input*weight 
-    n_output += n_bias
-    output_layer.append(n_output)
-print(output_layer)
-'''
